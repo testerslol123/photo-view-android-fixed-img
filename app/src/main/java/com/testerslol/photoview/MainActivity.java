ba19d31c4431a036f25437470ed3bddd6ccc07bf
Danny <testerslol123@gmail.com>
2019-09-16 00:56:28 +0700

    Initial Commit

---
+++ app/src/main/java/com/testerslol/photoview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.testerslol.photoview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.imagegallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<CreateList> createLists = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), createLists);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private ArrayList<CreateList> prepareData(){
+
+        ArrayList<CreateList> theimage = new ArrayList<>();
+        for(int i = 0; i< image_titles.length; i++){
+            CreateList createList = new CreateList();
+            createList.setImage_title(image_titles[i]);
+            createList.setImage_ID(image_ids[i]);
+            theimage.add(createList);
+        }
+        return theimage;
+    }
+}
+
